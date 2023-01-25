2d6001b5468223a8e0932c87d94aaf3fab0ee0d9
Danis <danyma7@gmail.com>
2023-01-25 20:19:32 +0500

    lesson 3

--- Lessons/les_03_median.py
+++ Lessons/les_03_median.py
@@ -3,19 +3,18 @@
 
 import numpy as np
 
-# z = np.array([100, 80, 75, 77, 89, 33, 45, 25, 65, 65])
-z = np.array([100, 80, 75, 77, 89, 33, 45, 25, 65])
+z = np.array([100, 80, 75, 77, 89, 33, 45, 25, 65, 65])
+# z = np.array([100, 80, 75, 77, 89, 33, 45, 25, 65])
 
-# z.shape
 z.sort()
-print(z)
 n = len(z)
-print(n)
 h = int(n / 2)
 
 if n % 2 == 0:
-    a = (z[h] + z[h + 1]) / 2
+    a = (z[h - 1] + z[h]) / 2
 else:
-    a = (z[h + 1])
+    a = (z[h])
 
-print('{:.3f}'.format(a))
+print(z)
+print(n)
+print(a)
--- Lessons/les_03_std_quartile.py
+++ Lessons/les_03_std_quartile.py
@@ -1,13 +1,27 @@
 # Квартили (quartile)
 # k - квартиль. Первый 25%, третий 75%
 # n - количество элементов
-# if (n * k / 100) integer 
+# Интерквартильное расстояние - отрезок,  равный разности 3-го и 1-го квартиля
+
 
 import numpy as np
 
-x = np.array([1, 2, 4, 2, 1, 5, 7, 2, 3, 5, 7, 8, 9])
-print(x)
+# z = np.array([1, 2, 4, 2, 1, 5, 7, 2, 3, 5, 7, 8, 9])
+z = np.array([1, 6, 4, 9, 1, 5, 7, 2, 3, 5, 9, 8])
+z.sort()
+n = len(z)
 
-print('{:.3f}'.format(np.std(x, ddof=1))) 
+# k = 25
+k = 75
+q = n * k / 100
+j = int(q)
 
-print('{:.3f}'.format(np.sqrt(np.var(x, ddof=1)))) 
+if q % 1 == 0:
+    a = (z[j - 1] + z[j]) / 2
+else:
+    a = (z[j])
+
+print(z)
+print(n)
+# print(q)
+print(a)
